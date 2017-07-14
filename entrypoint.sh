6a463dae01cf635392da094cb135d674430ba02e
Eve <eve@no-cloud.fr>
2017-07-14 16:13:56 +0200

    fix js path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i -e "s#{{HOST}}#${XMPP_HOST}#1" js/maestro-jsxc.js
-sed -i -e "s#{{DOMAIN}}#${XMPP_DOMAIN}#1" js/maestro-jsxc.js
+sed -i -e "s#{{HOST}}#${XMPP_HOST}#1" /usr/share/nginx/html/js/maestro-jsxc.js
+sed -i -e "s#{{DOMAIN}}#${XMPP_DOMAIN}#1" /usr/share/nginx/html/js/maestro-jsxc.js
 
 exec "$@"
